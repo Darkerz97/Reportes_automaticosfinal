4fa6dd4da0485b4a49bd55a03b6bd3e18f562771
Darkerz97 <darkers2010@gmail.com>
2025-03-19 08:26:56 -0600

    se agregan notas

--- main.py
+++ main.py
@@ -5,33 +5,43 @@ from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 
-#create a new document 
+#create a new document (instacia el documento para crear un documento en blanco)
 doc = Document()
 
+#titulo de la app
 st.write("Generador de reportes")
 
 
-# Initialize connection.
+# Initialize connection. establece conexion usando las credenciales de secrets
 conn = st.connection('mysql', type='sql')
-#query
-df = conn.query('select * from reparaciones_limpia;', ttl=600)
-#print results
 
+
+#query a la base de datos
+df = conn.query('select * from reparaciones_limpia;', ttl=600)
+
+#print results imprime resultados de la busqueda en la base 
 sbfolios= df['efolio']
 
+
+#Crea un menu desplegable con los folio en la base de dato
 Folio_selection= st.selectbox(
     'seleccione el folio para visualizar la informacion',
     (sbfolios))
 
+#imprime los datos de el folio seleccionado
 st.write(df[df['efolio']==Folio_selection])   
 
 
+
+
+#crea el documento y le agrega la informacion 
 # Add a title
 title = doc.add_heading(f'Reporte del Equipo {Folio_selection} ', level=1)
 title.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
 # Add a paragraph with bold and italic text
 paragraph = doc.add_paragraph('reporte de diganostico y reparacion')
+paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
 run = paragraph.runs[0]
 run.bold = True
 run.italic = True
@@ -48,17 +58,13 @@ list_paragraph.add_run('Bullet 2').bold = True
 list_paragraph.add_run(' - This is the second bullet point.')
 
 
-
+#guarda el documento
 doc.save('demo.docx')
 
 
 
 
-
-
-
-
-
+#boton descargable para el documento
 with open("demo.docx","rb") as file:
     st.download_button(
         label="Descargar Reporte",
